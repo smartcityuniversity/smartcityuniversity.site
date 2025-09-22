b7e22dd5baab94279a7ae2430568c9cc0eccc52f
smartcityuniversity <adediresamuel08@gmail.com>
2025-09-22 08:04:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling for internal links
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click',e=>{
+    const id = a.getAttribute('href');
+    if(id.length>1){
+      e.preventDefault();
+      document.querySelector(id).scrollIntoView({behavior:'smooth',block:'start'});
+    }
+  });
+});
